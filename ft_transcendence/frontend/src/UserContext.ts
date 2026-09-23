96a6c5fabb6d6e76d290e8ec41d7891c9369e8fa
Maram <xmaram42@gmail.com>
2026-09-23 08:37:32 +0000

    o

--- ft_transcendence/frontend/src/UserContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export type User = {
-  id: string;
-  email: string;
-  username: string;
-};
-
-export const UserContext = createContext<User | null>(null);
